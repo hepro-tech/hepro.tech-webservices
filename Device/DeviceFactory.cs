f279d52ccea21b8fab309e3342e444e27230e544
George Antonious <george@antonious.ca>
2018-02-18 00:18:59 -0700

    Login in the DeviceFactory

--- Device/DeviceFactory.cs
+++ Device/DeviceFactory.cs
@@ -1,10 +1,19 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Particle.SDK;
 
 namespace HeProTech.Webservices.Device
 {
     public class DeviceFactory
     {
+        public DeviceFactory(IConfiguration configuration)
+        {
+            var username = configuration["PARTICLE_CLOUD_USER"];
+            var passowrd = configuration["PARTICLE_CLOUD_PASSWORD"];
+            ParticleCloud.SharedCloud.LoginAsync(username, passowrd).Wait(); // wapoz lets not follow async paradigms ¯\_(ツ)_/¯
+        }
+
         public async Task<ParticleDevice> GetDeviceAsync()
         {
             var devices = await ParticleCloud.SharedCloud.GetDevicesAsync();
