486cebd2bc3299ea51ffe1b9b3b50cdcbdd0e8a2
George Antonious <george@antonious.ca>
2018-02-18 00:23:16 -0700

    Add the ability to broadcast notifications

--- Notifcations/NotificationManager.cs
+++ Notifcations/NotificationManager.cs
@@ -1,15 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using OneSignal.CSharp.SDK;
+using OneSignal.CSharp.SDK.Resources;
+using OneSignal.CSharp.SDK.Resources.Notifications;
 
 namespace HeProTech.Webservices.Notifications
 {
     public class NotificationManager
     {
+        private readonly string _appId;
         private readonly OneSignalClient _notificationClient;
 
         public NotificationManager(IConfiguration configuration)
         {
-            _notificationClient = new OneSignalClient("ONE_CLIENT_API_KEY");
+            _notificationClient = new OneSignalClient(configuration["ONE_CLIENT_API_KEY"]);
+            _appId = configuration["ONE_CLIENT_APP_ID"];
+        }
+
+        public async Task BroadcastNotificationAsync(string notifcationMessage)
+        {
+            var options = new NotificationCreateOptions();
+            options.AppId = Guid.Parse(_appId);
+            options.IncludedSegments = new List<string> { "All" };
+            options.Contents.Add(LanguageCodes.English, notifcationMessage);
+
+            await Task.Run(() => _notificationClient.Notifications.Create(options));
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HeProTech.Webservices.Device;
+using HeProTech.Webservices.Notifications;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -29,6 +30,7 @@ namespace HeProTech.Webservices
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<NotificationManager>();
             services.AddSingleton<DeviceFactory>();
             services.AddSingleton<DeviceManager>();
             services.AddMvc();
